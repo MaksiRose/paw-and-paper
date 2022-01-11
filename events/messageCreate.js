fa5abc17dd025d7847aa7f86c901dcb536c876d7
maksi <maksilucy1@gmail.com>
2022-01-11 23:21:39 +0100

    update profileData

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -205,6 +205,14 @@ module.exports = {
 
 					if (profileData && usersActiveCommandsAmountMap.get(message.author.id).activeCommands <= 0) {
 
+						profileData = await profileModel
+							.findOne({
+								userId: message.author.id,
+								serverId: message.guild.id,
+							}).catch(async (error) => {
+								return await errorHandling.output(message, error);
+							});
+
 						automaticCooldownTimeoutArray[message.author.id] = setTimeout(async function() {
 
 							console.log(`\x1b[32m\x1b[0m${message.author.tag} (${message.author.id}): hasCooldown changed from \x1b[33m${profileData.hasCooldown} \x1b[0mto \x1b[33mfalse \x1b[0min \x1b[32m${message.guild.name} \x1b[0mat \x1b[3m${new Date().toLocaleString()} \x1b[0m`);
