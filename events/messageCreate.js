af03387c4af4349eb880a0be555725d6fdc81ea0
maksi <maksilucy1@gmail.com>
2022-01-11 23:08:06 +0100

    removed unneccessary code

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -229,20 +229,6 @@ module.exports = {
 			await errorHandling.output(message, error);
 		}
 
-		if (profileData) {
-
-			console.log(`\x1b[32m\x1b[0m${message.author.tag} (${message.author.id}): hasCooldown changed from \x1b[33m${profileData.hasCooldown} \x1b[0mto \x1b[33mtrue \x1b[0min \x1b[32m${message.guild.name} \x1b[0mat \x1b[3m${new Date().toLocaleString()} \x1b[0m`);
-			profileData = await profileModel
-				.findOneAndUpdate(
-					{ userId: message.author.id, serverId: message.guild.id },
-					{ $set: { hasCooldown: true } },
-					{ new: true },
-				)
-				.catch(async (error) => {
-					return await errorHandling.output(message, error);
-				});
-		}
-
 		automaticRestingTimeoutArray[message.author.id] = setTimeout(automaticRestingTimeoutFunction, 600000);
 
 		async function automaticRestingTimeoutFunction() {
