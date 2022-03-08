706a6687e6522aff25ee775128f08340bd53207b
maksirose <maksilucy1@gmail.com>
2022-03-08 15:42:59 +0100

    adds cooldown and resting failsafe

--- paw.js
+++ paw.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const fs = require('fs');
+const profileModel = require('./models/profileModel');
 
 const client = new Discord.Client({
 	intents: [
@@ -21,6 +22,25 @@ client.commands = new Discord.Collection();
 for (const file of fs.readdirSync('./handlers/')) {
 
 	require(`./handlers/${file}`).execute(client);
+}
+
+for (const account of profileModel.find({})) {
+
+	if (account.hasCooldown == true) {
+
+		profileModel.findOneAndUpdate(
+			{ userId: account.userId, serverId: account.serverId },
+			{ $set: { hasCooldown: false } },
+		);
+	}
+
+	if (account.isResting == true) {
+
+		profileModel.findOneAndUpdate(
+			{ userId: account.userId, serverId: account.serverId },
+			{ $set: { isResting: false, energy: account.maxEnergy } },
+		);
+	}
 }
 
 client.login(config.token);
