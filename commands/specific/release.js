3dacd69aee2a4dbf4b1839b04faff95871917c9b
MaksiRose <maksilucy1@gmail.com>
2022-03-14 14:04:29 +0100

    fixes #161

---
+++ commands/specific/release.js
@@ -0,0 +1,74 @@
+const config = require('../../config.json');
+const profileModel = require('../../models/profileModel');
+
+module.exports = {
+	name: 'release',
+	async sendMessage(client, message, argumentsArray) {
+
+		if (message.author.id != '268402976844939266') {
+
+			return;
+		}
+
+		if (!argumentsArray) {
+
+			return;
+		}
+
+		const allProfiles = [...new Set((await profileModel
+			.find({}))
+			.map(user => user.userId))];
+
+		for (const userId of allProfiles) {
+
+			const user = await client.users.fetch(userId);
+
+			await user
+				.createDM()
+				.catch((error) => {
+					if (error.httpStatus !== 404) {
+						throw new Error(error);
+					}
+				});
+
+			const oldMessages = await user.dmChannel.messages.fetch({ limit: 100 });
+
+			for (const [, msg] of oldMessages) {
+
+				if (msg.author.bot && msg.components.length > 0) {
+
+					await msg
+						.edit({ components: [] })
+						.catch((error) => {
+							if (error.httpStatus !== 404) {
+								throw new Error(error);
+							}
+						});
+				}
+			}
+
+			await user
+				.send({
+					embeds: [{
+						color: config.default_color,
+						description: argumentsArray.join(' '),
+						footer: { text: 'You can change if you want updates for new releases by clicking the button below.' },
+					}],
+					components: [{
+						type: 'ACTION_ROW',
+						components: [{
+							type: 'BUTTON',
+							customId: 'updates-off',
+							label: 'Turn updates off',
+							style: 'SECONDARY',
+						}],
+					}],
+				})
+				.catch((error) => {
+					if (error.httpStatus !== 404) {
+						throw new Error(error);
+					}
+				});
+		}
+	},
+};
