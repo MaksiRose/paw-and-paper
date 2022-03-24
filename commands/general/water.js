a4d0f101b039538b9107d10e1e38ead901dd8055
maksirose <maksilucy1@gmail.com>
2022-03-24 19:12:45 +0100

    adds all necessary texts

--- commands/general/water.js
+++ commands/general/water.js
@@ -5,6 +5,7 @@ const { remindOfAttack } = require('../specific/attack');
 const profileModel = require('../../models/profileModel');
 const { pullFromWeightedTable } = require('../../utils/randomizers');
 const { checkLevelUp } = require('../../utils/levelHandling');
+const { pronounAndPlural } = require('../../utils/getPronouns');
 
 const oneMinute = 60000;
 const thirtyMinutes = oneMinute * 30;
@@ -34,7 +35,7 @@ module.exports = {
 			embedArray.push({
 				color: profileData.color,
 				author: { name: profileData.name, icon_url: profileData.avatarURL },
-				description: `*${profileData.name} has no gingko tree to water!* PLACEHOLDER`,
+				description: `*${profileData.name} has already fetched water when ${pronounAndPlural(profileData, 0, 'remember')} that ${pronounAndPlural(profileData, 0, 'has', 'have')} nothing to water.*`,
 			});
 
 			return await message
@@ -68,8 +69,8 @@ module.exports = {
 			embedArray.push({
 				color: profileData.color,
 				author: { name: profileData.name, icon_url: profileData.avatarURL },
-				description: `*${profileData.name} is perfectly in time!* PLACEHOLDER`,
-				footer: { text: `+${experiencePoints} XP (${profileData.experience + experiencePoints}/${profileData.levels * 50})${healthPoints > 0 ? `\n+${healthPoints} health (${profileData.health + healthPoints}/${profileData.maxEnergy})` : ''}\n\n+${saplingHealthPoints} health for gingko sapling\nCome back to water it in 24 hours.` },
+				description: `*${profileData.name} waters the seedling, and it look it's at the perfect time. The ginkgo tree looks healthy, the leaves have a strong green color, and a pleasant fragrance emanates from them. The ${profileData.species} feels warm and safe from the scent.*`,
+				footer: { text: `+${experiencePoints} XP (${profileData.experience + experiencePoints}/${profileData.levels * 50})${healthPoints > 0 ? `\n+${healthPoints} health (${profileData.health + healthPoints}/${profileData.maxEnergy})` : ''}\n\n+${saplingHealthPoints} health for ginkgo sapling\nCome back to water it in 24 hours.` },
 			});
 		}
 		else if (timeDifference >= -threeHours && timeDifference <= threeHours) {
@@ -80,8 +81,8 @@ module.exports = {
 			embedArray.push({
 				color: profileData.color,
 				author: { name: profileData.name, icon_url: profileData.avatarURL },
-				description: `*${profileData.name} is just in time!* PLACEHOLDER`,
-				footer: { text: `+${experiencePoints} XP (${profileData.experience + experiencePoints}/${profileData.levels * 50})\n\nCome back to water the gingko sapling in 24 hours.` },
+				description: `*${profileData.name} waters the seedling, and it look like the sapling needs it. Although the ginkgo tree looks healthy, with leaves of beautiful green color and a light scent, the soil seems to be already quite dry.*`,
+				footer: { text: `+${experiencePoints} XP (${profileData.experience + experiencePoints}/${profileData.levels * 50})\n\nCome back to water the ginkgo sapling in 24 hours.` },
 			});
 		}
 		else if (timeDifference < -threeHours) {
@@ -92,8 +93,8 @@ module.exports = {
 			embedArray.push({
 				color: profileData.color,
 				author: { name: profileData.name, icon_url: profileData.avatarURL },
-				description: `*${profileData.name} is too soon!* PLACEHOLDER`,
-				footer: { text: `${saplingHealthPoints} health for gingko tree\nCome back to water it in 24 hours.` },
+				description: `*The soil is already soggy when ${profileData.name} adds more water to it. The leaves are yellow-brown, the stem is muddy and has a slight mold. Next time the ${profileData.species} should wait a little with the watering.*`,
+				footer: { text: `${saplingHealthPoints} health for ginkgo tree\nCome back to water it in 24 hours.` },
 			});
 		}
 		else {
@@ -105,8 +106,8 @@ module.exports = {
 			embedArray.push({
 				color: profileData.color,
 				author: { name: profileData.name, icon_url: profileData.avatarURL },
-				description: `*${profileData.name} decides to see if the gingko tree needs watering, and sure enough: the leaves are drooping, some have lost color, and many of them fell on the ground. It is about time that the poor tree gets some water.*`,
-				footer: { text: `-${saplingHealthPoints} health for gingko tree\nCome back to water it in 24 hours.` },
+				description: `*${profileData.name} decides to see if the ginkgo tree needs watering, and sure enough: the leaves are drooping, some have lost color, and many of them fell on the ground. It is about time that the poor tree gets some water.*`,
+				footer: { text: `-${saplingHealthPoints} health for ginkgo tree\nCome back to water it in 24 hours.` },
 			});
 		}
 
@@ -148,7 +149,7 @@ module.exports = {
 					embeds: [{
 						color: profileData.color,
 						author: { name: profileData.name, icon_url: profileData.avatarURL },
-						description: `*${profileData.name}'s gingko tree died!* PLACEHOLDER`,
+						description: `*No matter what ${profileData.name} does, all the leaves on the ginkgo tree have either fallen off, or are dark brown and hang limply. It's time to say goodbye to the tree.*`,
 					}],
 				})
 				.catch((error) => {
