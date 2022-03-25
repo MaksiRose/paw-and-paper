71131034801ac0e9ed4f62ae54b5d4e656c03235
maksirose <maksilucy1@gmail.com>
2022-03-25 22:34:44 +0100

    changes internal command folder system

---
+++ commands/maintenance/eat.js
@@ -0,0 +1,326 @@
+const profileModel = require('../../models/profileModel');
+const serverModel = require('../../models/serverModel');
+const startCooldown = require('../../utils/startCooldown');
+const { generateRandomNumber } = require('../../utils/randomizers');
+const { commonPlantsMap, uncommonPlantsMap, rarePlantsMap, speciesMap } = require('../../utils/itemsInfo');
+const { hasNotCompletedAccount } = require('../../utils/checkAccountCompletion');
+const { isInvalid } = require('../../utils/checkValidity');
+const { sendMessage } = require('./inventory');
+const { remindOfAttack } = require('../gameplay/attack');
+const { pronounAndPlural, pronoun, upperCasePronounAndPlural } = require('../../utils/getPronouns');
+const blockEntrance = require('../../utils/blockEntrance');
+
+module.exports = {
+	name: 'eat',
+	async sendMessage(client, message, argumentsArray, profileData, serverData, embedArray) {
+
+		if (await hasNotCompletedAccount(message, profileData)) {
+
+			return;
+		}
+
+		if (await isInvalid(message, profileData, embedArray, [module.exports.name])) {
+
+			return;
+		}
+
+		profileData = await startCooldown(message, profileData);
+		const messageContent = remindOfAttack(message);
+
+		if (profileData.hunger >= 100) {
+
+			embedArray.push({
+				color: profileData.color,
+				author: { name: profileData.name, icon_url: profileData.avatarURL },
+				description: `*${profileData.name}'s stomach bloats as ${pronounAndPlural(profileData, 0, 'roll')} around camp, stuffing food into ${pronoun(profileData, 2)} mouth. The ${profileData.species} might need to take a break from food before ${pronounAndPlural(profileData, 0, 'goes', 'go')} into a food coma.*`,
+			});
+
+			return await message
+				.reply({
+					content: messageContent,
+					embeds: embedArray,
+					failIfNotExists: false,
+				})
+				.catch((error) => {
+					if (error.httpStatus !== 404) {
+						throw new Error(error);
+					}
+				});
+		}
+
+		if ((profileData.rank !== 'Youngling' && serverData.blockedEntranceObject.den === null && generateRandomNumber(20, 0) === 0) || serverData.blockedEntranceObject.den === 'food den') {
+
+			return await blockEntrance(message, messageContent, profileData, 'food den');
+		}
+
+		if (!argumentsArray.length) {
+
+			// I have to call the inventory command directly here instead of executing messageCreate.js, since doing otherwise would always return profileData.hasCooldown as true
+			return await sendMessage(client, message, argumentsArray, profileData, serverData, embedArray);
+		}
+
+		const chosenFood = argumentsArray.join(' ');
+		let finalHungerPoints = 0;
+		let minimumHungerPoints = 0;
+		let finalHealthPoints = 0;
+		let minimumHealthPoints = 0;
+		let finalEnergyPoints = 0;
+
+		const embed = {
+			color: profileData.color,
+			author: { name: profileData.name, icon_url: profileData.avatarURL },
+			description: '',
+			footer: { text: '' },
+		};
+
+		const allPlantMaps = new Map([...commonPlantsMap, ...uncommonPlantsMap, ...rarePlantsMap]);
+		if (allPlantMaps.has(chosenFood) == true) {
+
+			let plantType;
+			let plantMap;
+
+			if (commonPlantsMap.has(chosenFood) == true) {
+
+				plantType = 'commonPlants';
+				plantMap = new Map([...commonPlantsMap]);
+			}
+
+			if (uncommonPlantsMap.has(chosenFood) == true) {
+
+				plantType = 'uncommonPlants';
+				plantMap = new Map([...uncommonPlantsMap]);
+			}
+
+			if (rarePlantsMap.has(chosenFood) == true) {
+
+				plantType = 'rarePlants';
+				plantMap = new Map([...rarePlantsMap]);
+			}
+
+			if (serverData.inventoryObject[plantType][chosenFood] <= 0) {
+
+				embedArray.push({
+					color: profileData.color,
+					author: { name: profileData.name, icon_url: profileData.avatarURL },
+					description: `*${profileData.name} searches for a ${chosenFood} all over the pack, but couldn't find one...*`,
+				});
+
+				return await message
+					.reply({
+						content: messageContent,
+						embeds: embedArray,
+						failIfNotExists: false,
+					})
+					.catch((error) => {
+						if (error.httpStatus !== 404) {
+							throw new Error(error);
+						}
+					});
+			}
+
+			if (plantMap.get(chosenFood).edibality == 't') {
+
+				minimumHungerPoints = -5;
+				minimumHealthPoints = -10;
+				finalHungerPoints = generateRandomNumber(5, minimumHungerPoints);
+				finalHealthPoints = generateRandomNumber(3, minimumHealthPoints);
+
+				embed.description = `*A yucky feeling drifts down ${profileData.name}'s throat. ${upperCasePronounAndPlural(profileData, 0, 'shakes and spits', 'shake and spit')} it out, trying to rid ${pronoun(profileData, 2)} mouth of the taste. The plant is poisonous!*`;
+			}
+
+			if (plantMap.get(chosenFood).edibality == 'i') {
+
+				minimumHungerPoints = -1;
+				finalHungerPoints = generateRandomNumber(3, minimumHungerPoints);
+
+				embed.description = `*${profileData.name} slowly opens ${pronoun(profileData, 2)} mouth and chomps onto the ${chosenFood}. The ${profileData.species} swallows it, but ${pronoun(profileData, 2)} face has a look of disgust. That wasn't very tasty!*`;
+			}
+
+			if (plantMap.get(chosenFood).edibality == 'e') {
+
+				if (speciesMap.get(profileData.species).diet == 'carnivore') {
+
+					minimumHungerPoints = 1;
+					finalHungerPoints = generateRandomNumber(5, minimumHungerPoints);
+
+					embed.description = `*${profileData.name} plucks a ${chosenFood} from the pack storage and nibbles away at it. It has a bitter, foreign taste, not the usual meaty meal the ${profileData.species} prefers.*`;
+				}
+
+				if (speciesMap.get(profileData.species).diet == 'herbivore' || speciesMap.get(profileData.species).diet == 'omnivore') {
+
+					minimumHungerPoints = 11;
+					finalHungerPoints = generateRandomNumber(10, minimumHungerPoints);
+
+					embed.description = `*Leaves flutter into the storage den, landing near ${profileData.name}'s feet. The ${profileData.species} searches around the inventory determined to find the perfect meal, and that ${pronounAndPlural(profileData, 0, 'does', 'do')}. ${profileData.name} plucks a ${chosenFood} from the pile and eats until ${pronoun(profileData, 2)} stomach is pleased.*`;
+				}
+			}
+
+			if (plantMap.get(chosenFood).givesEnergy == true) {
+
+				finalEnergyPoints = 20;
+
+				if (profileData.energy + finalEnergyPoints > profileData.maxEnergy) {
+
+					finalEnergyPoints -= (profileData.energy + finalEnergyPoints) - profileData.maxEnergy;
+				}
+			}
+
+			if (profileData.health + finalHealthPoints < 0) {
+
+				finalHealthPoints = -profileData.health;
+			}
+
+			if (profileData.hunger + finalHungerPoints > profileData.maxHunger) {
+
+				finalHungerPoints -= (profileData.hunger + finalHungerPoints) - profileData.maxHunger;
+			}
+
+			serverData.inventoryObject[plantType][chosenFood] -= 1;
+
+			embed.footer.text = `+${finalHungerPoints} hunger (${finalHungerPoints + profileData.hunger}/${profileData.maxHunger})`;
+
+			if (plantMap[chosenFood] == true) {
+
+				embed.footer.text += `\n+${finalEnergyPoints} energy (${finalEnergyPoints + profileData.energy}/${profileData.maxHunger})`;
+			}
+
+			if (plantMap[chosenFood] == 't') {
+
+				embed.footer.text += `\n${finalHealthPoints} health (${finalHealthPoints + profileData.health}/${profileData.maxHealth})`;
+			}
+
+			embed.footer.text += `${(profileData.currentRegion != 'food den') ? '\nYou are now at the food den' : ''}\n\n-1 ${chosenFood} for ${message.guild.name}`;
+
+			profileData = await profileModel.findOneAndUpdate(
+				{ userId: message.author.id, serverId: message.guild.id },
+				{
+					$inc: {
+						hunger: +finalHungerPoints,
+						energy: +finalEnergyPoints,
+						health: +finalHealthPoints,
+					},
+					$set: { currentRegion: 'food den' },
+				},
+			);
+
+			serverData = await serverModel.findOneAndUpdate(
+				{ serverId: message.guild.id },
+				{ $set: { inventoryObject: serverData.inventoryObject } },
+			);
+
+			embedArray.push(embed);
+			return await message
+				.reply({
+					content: messageContent,
+					embeds: embedArray,
+					failIfNotExists: false,
+				})
+				.catch((error) => {
+					if (error.httpStatus !== 404) {
+						throw new Error(error);
+					}
+				});
+		}
+
+		if (speciesMap.has(chosenFood) == true) {
+
+			if (serverData.inventoryObject.meat[chosenFood] <= 0) {
+
+				embedArray.push({
+					color: profileData.color,
+					author: { name: profileData.name, icon_url: profileData.avatarURL },
+					description: `*${profileData.name} searches for a ${chosenFood} all over the pack, but couldn't find one...*`,
+				});
+
+				return await message
+					.reply({
+						content: messageContent,
+						embeds: embedArray,
+						failIfNotExists: false,
+					})
+					.catch((error) => {
+						if (error.httpStatus !== 404) {
+							throw new Error(error);
+						}
+					});
+			}
+
+			if (speciesMap.get(profileData.species).diet == 'herbivore') {
+
+				minimumHungerPoints = 1;
+				finalHungerPoints = generateRandomNumber(5, minimumHungerPoints);
+
+				embed.description = `*${profileData.name} stands by the storage den, eyeing the varieties of food. A ${chosenFood} catches ${pronoun(profileData, 2)} attention. The ${profileData.species} walks over to it and begins to eat.* "This isn't very good!" *${profileData.name} whispers to ${pronoun(profileData, 4)} and leaves the den, stomach still growling, and craving for plants to grow.*`;
+			}
+
+			if (speciesMap.get(profileData.species).diet == 'carnivore' || speciesMap.get(profileData.species).diet == 'omnivore') {
+
+				minimumHungerPoints = 11;
+				finalHungerPoints = generateRandomNumber(10, minimumHungerPoints);
+
+				embed.description = `*${profileData.name} sits chewing maliciously on a ${chosenFood}. A dribble of blood escapes out of ${pronoun(profileData, 2)} jaw as the ${profileData.species} finishes off the meal. It was a delicious feast, but very messy!*`;
+			}
+
+			if (profileData.hunger + finalHungerPoints > profileData.maxHunger) {
+
+				finalHungerPoints -= (profileData.hunger + finalHungerPoints) - profileData.maxHunger;
+			}
+
+			serverData.inventoryObject.meat[chosenFood] -= 1;
+
+			profileData = await profileModel.findOneAndUpdate(
+				{ userId: message.author.id, serverId: message.guild.id },
+				{ $inc: { hunger: +finalHungerPoints } },
+			);
+
+			serverData = await serverModel.findOneAndUpdate(
+				{ serverId: message.guild.id },
+				{ $set: { inventoryObject: serverData.inventoryObject } },
+			);
+
+			embed.footer.text = `+${finalHungerPoints} hunger (${profileData.hunger}/${profileData.maxHunger})${(profileData.currentRegion != 'food den') ? '\nYou are now at the food den' : ''}\n\n-1 ${chosenFood} for ${message.guild.name}`;
+
+			embedArray.push(embed);
+			return await message
+				.reply({
+					content: messageContent,
+					embeds: embedArray,
+					failIfNotExists: false,
+				})
+				.catch((error) => {
+					if (error.httpStatus !== 404) {
+						throw new Error(error);
+					}
+				});
+		}
+
+		if (message.mentions.users.size > 0) {
+
+			const taggedProfileData = await profileModel.findOne({
+				userId: message.mentions.users.first().id,
+			});
+
+			if (taggedProfileData) {
+
+				embed.description = `*${profileData.name} looks down at ${taggedProfileData.name} as ${pronounAndPlural(profileData, 0, 'nom')} on the ${taggedProfileData.species}'s leg.* "No eating packmates here!" *${taggedProfileData.name} chuckled, shaking off ${profileData.name}.*`;
+				embed.footer.text = (profileData.currentRegion != 'food den') ? '\nYou are now at the food den' : '';
+
+				embedArray.push(embed);
+				return await message
+					.reply({
+						content: messageContent,
+						embeds: embedArray,
+						failIfNotExists: false,
+					})
+					.catch((error) => {
+						if (error.httpStatus !== 404) {
+							throw new Error(error);
+						}
+					});
+			}
+		}
+
+		// I have to call the inventory command directly here instead of executing messageCreate.js, since doing otherwise would always return profileData.hasCooldown as true
+		return await sendMessage(client, message, argumentsArray, profileData, serverData, embedArray);
+	},
+};
---
+++ commands/maintenance/water.js
@@ -0,0 +1,169 @@
+const { hasNotCompletedAccount } = require('../../utils/checkAccountCompletion');
+const { isInvalid } = require('../../utils/checkValidity');
+const startCooldown = require('../../utils/startCooldown');
+const { remindOfAttack } = require('../gameplay/attack');
+const profileModel = require('../../models/profileModel');
+const { pullFromWeightedTable } = require('../../utils/randomizers');
+const { checkLevelUp } = require('../../utils/levelHandling');
+const { pronounAndPlural } = require('../../utils/getPronouns');
+
+const oneMinute = 60000;
+const thirtyMinutes = oneMinute * 30;
+const oneHour = thirtyMinutes * 2;
+const threeHours = oneHour * 3;
+const twentyFourHours = threeHours * 8;
+
+module.exports = {
+	name: 'water',
+	async sendMessage(client, message, argumentsArray, profileData, serverData, embedArray) {
+
+		if (await hasNotCompletedAccount(message, profileData)) {
+
+			return;
+		}
+
+		if (await isInvalid(message, profileData, embedArray, [module.exports.name])) {
+
+			return;
+		}
+
+		profileData = await startCooldown(message, profileData);
+		const messageContent = remindOfAttack(message);
+
+		if (profileData.saplingObject.exists === false) {
+
+			embedArray.push({
+				color: profileData.color,
+				author: { name: profileData.name, icon_url: profileData.avatarURL },
+				description: `*${profileData.name} has already fetched water when ${pronounAndPlural(profileData, 0, 'remember')} that ${pronounAndPlural(profileData, 0, 'has', 'have')} nothing to water.*`,
+			});
+
+			return await message
+				.reply({
+					content: messageContent,
+					embeds: embedArray,
+					failIfNotExists: false,
+				})
+				.catch((error) => {
+					if (error.httpStatus !== 404) {
+						throw new Error(error);
+					}
+				});
+		}
+
+		const saplingObject = { ...profileData.saplingObject };
+		const timeDifference = Date.now() - saplingObject.nextWaterTimestamp;
+		const timeDifferenceInMinutes = timeDifference / oneMinute;
+
+		let experiencePoints = 0;
+		let healthPoints = 0;
+
+		if (timeDifference >= -thirtyMinutes && timeDifference <= thirtyMinutes) {
+
+			const saplingHealthPoints = 4 - Math.round(timeDifferenceInMinutes / 10);
+			saplingObject.health += saplingHealthPoints;
+			saplingObject.waterCycles += 1;
+
+			experiencePoints = saplingObject.waterCycles * 2;
+			healthPoints = pullFromWeightedTable({ 0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6 });
+
+			embedArray.push({
+				color: profileData.color,
+				author: { name: profileData.name, icon_url: profileData.avatarURL },
+				description: `*${profileData.name} waters the seedling, and it look it's at the perfect time. The ginkgo tree looks healthy, the leaves have a strong green color, and a pleasant fragrance emanates from them. The ${profileData.species} feels warm and safe from the scent.*`,
+				footer: { text: `+${experiencePoints} XP (${profileData.experience + experiencePoints}/${profileData.levels * 50})${healthPoints > 0 ? `\n+${healthPoints} health (${profileData.health + healthPoints}/${profileData.maxEnergy})` : ''}\n\n+${saplingHealthPoints} health for ginkgo sapling\nCome back to water it in 24 hours.` },
+			});
+		}
+		else if (timeDifference >= -threeHours && timeDifference <= threeHours) {
+
+			saplingObject.waterCycles += 1;
+			experiencePoints = saplingObject.waterCycles * 2;
+
+			embedArray.push({
+				color: profileData.color,
+				author: { name: profileData.name, icon_url: profileData.avatarURL },
+				description: `*${profileData.name} waters the seedling, and it look like the sapling needs it. Although the ginkgo tree looks healthy, with leaves of beautiful green color and a light scent, the soil seems to be already quite dry.*`,
+				footer: { text: `+${experiencePoints} XP (${profileData.experience + experiencePoints}/${profileData.levels * 50})\n\nCome back to water the ginkgo sapling in 24 hours.` },
+			});
+		}
+		else if (timeDifference < -threeHours) {
+
+			const saplingHealthPoints = Math.floor((timeDifferenceInMinutes + 180) / 60);
+			saplingObject.health += saplingHealthPoints;
+
+			embedArray.push({
+				color: profileData.color,
+				author: { name: profileData.name, icon_url: profileData.avatarURL },
+				description: `*The soil is already soggy when ${profileData.name} adds more water to it. The leaves are yellow-brown, the stem is muddy and has a slight mold. Next time the ${profileData.species} should wait a little with the watering.*`,
+				footer: { text: `${saplingHealthPoints} health for ginkgo tree\nCome back to water it in 24 hours.` },
+			});
+		}
+		else {
+
+			const overdueHours = Math.ceil(timeDifference / oneHour);
+			const saplingHealthPoints = ((overdueHours * (overdueHours + 1)) / 2) + (Math.round(saplingObject.waterCycles / 10) * overdueHours);
+			saplingObject.health -= saplingHealthPoints;
+
+			embedArray.push({
+				color: profileData.color,
+				author: { name: profileData.name, icon_url: profileData.avatarURL },
+				description: `*${profileData.name} decides to see if the ginkgo tree needs watering, and sure enough: the leaves are drooping, some have lost color, and many of them fell on the ground. It is about time that the poor tree gets some water.*`,
+				footer: { text: `-${saplingHealthPoints} health for ginkgo tree\nCome back to water it in 24 hours.` },
+			});
+		}
+
+		saplingObject.nextWaterTimestamp = Date.now() + twentyFourHours;
+
+		if (profileData.health + healthPoints > profileData.maxHealth) {
+
+			healthPoints -= (profileData.health + healthPoints) - profileData.maxThirst;
+		}
+
+		profileData = await profileModel.findOneAndUpdate(
+			{ userId: message.author.id, serverId: message.guild.id },
+			{
+				$set: { saplingObject: saplingObject },
+				$inc: {
+					experience: experiencePoints,
+					health: healthPoints,
+				},
+			},
+		);
+
+		const botReply = await message
+			.reply({
+				content: messageContent,
+				embeds: embedArray,
+				failIfNotExists: false,
+			})
+			.catch((error) => {
+				if (error.httpStatus !== 404) {
+					throw new Error(error);
+				}
+			});
+
+		await checkLevelUp(profileData, botReply);
+
+		if (profileData.saplingObject.health <= 0) {
+
+			await message.channel
+				.send({
+					embeds: [{
+						color: profileData.color,
+						author: { name: profileData.name, icon_url: profileData.avatarURL },
+						description: `*No matter what ${profileData.name} does, all the leaves on the ginkgo tree have either fallen off, or are dark brown and hang limply. It's time to say goodbye to the tree.*`,
+					}],
+				})
+				.catch((error) => {
+					if (error.httpStatus !== 404) {
+						throw new Error(error);
+					}
+				});
+
+			await profileModel.findOneAndUpdate(
+				{ userId: profileData.userId, serverId: profileData.serverId },
+				{ $set: { saplingObject: { exists: false, health: 50, waterCycles: 0, nextWaterTimestamp: null } } },
+			);
+		}
+	},
+};
