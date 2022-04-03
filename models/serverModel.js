6ccf5d1d1f87dc8129eb3cb866bd3efa8ec571f5
maksirose <maksilucy1@gmail.com>
2022-04-03 13:26:30 +0200

    Define schemas in model files

--- models/serverModel.js
+++ models/serverModel.js
@@ -1,3 +1,22 @@
-const modelConstructor = require('./modelConstructor');
-const model = new modelConstructor.model('./database/servers');
+const constructor = require('./constructor');
+const { commonPlantsMap, uncommonPlantsMap, rarePlantsMap, speciesMap } = require('../utils/itemsInfo');
+
+const schema = new constructor.schema({
+	serverId: { type: ['string'], locked: true },
+	name: { type: ['string'] },
+	inventoryObject: { type: ['object', ['object', ['number']]], default: {
+		commonPlants: Object.fromEntries([...commonPlantsMap.keys()].sort().map(key => [key, 0])),
+		uncommonPlants: Object.fromEntries([...uncommonPlantsMap.keys()].sort().map(key => [key, 0])),
+		rarePlants: Object.fromEntries([...rarePlantsMap.keys()].sort().map(key => [key, 0])),
+		meat: Object.fromEntries([...speciesMap.keys()].sort().map(key => [key, 0])),
+	} },
+	blockedEntranceObject: { type: ['object', ['any']], default: { den: null, blockedKind: null } },
+	activeUsersArray: { type: ['array', ['string']] },
+	nextPossibleAttack: { type: ['number'], default: Date.now() },
+	visitChannelId: { type: ['any'] },
+	currentlyVisiting: { type: ['any'] },
+	shop: { type: ['array', ['object', ['string', 'number']]] },
+});
+
+const model = new constructor.model('./database/servers', schema);
 module.exports = model;
