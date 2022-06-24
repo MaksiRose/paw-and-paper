2b8ae46e947a39182292530f4db3dac8bf07f982
maksirose <maksilucy1@gmail.com>
2022-06-24 12:37:03 +0200

    Fix #427

--- commands/interaction/hug.js
+++ commands/interaction/hug.js
@@ -1,7 +1,7 @@
 // @ts-check
 const { generateRandomNumber } = require('../../utils/randomizers');
 const { error_color } = require('../../config.json');
-const { MessageActionRow, MessageButton } = require('discord.js');
+const { MessageActionRow, MessageButton, Message } = require('discord.js');
 const disableAllComponents = require('../../utils/disableAllComponents');
 const profileModel = require('../../models/profileModel');
 const { addFriendshipPoints } = require('../../utils/friendshipHandling');
@@ -70,7 +70,7 @@ module.exports.sendMessage = async (client, message, argumentsArray, userData, s
 		return;
 	}
 
-	const botReply = await message
+	await message
 		.reply({
 			embeds: [...embedArray, {
 				color: characterData?.color || message.member?.displayColor || message.author.accentColor || '#ffffff',
@@ -95,75 +95,86 @@ module.exports.sendMessage = async (client, message, argumentsArray, userData, s
 			failIfNotExists: false,
 		})
 		.catch((error) => { throw new Error(error); });
+};
 
-	const filter = (/** @type {import('discord.js').MessageComponentInteraction} */ i) => i.user.id === firstMentionedUser.id && i.customId.includes('hug');
+/**
+ * A function that is called when the user accepts the hug. It edits the message to show a hug gif and
+ * adds friendship points.
+ * @param {import('discord.js').ButtonInteraction} interaction
+ * @param {import('../../typedef').ProfileSchema} userData
+ * @param {import('../../typedef').Character} characterData
+ * @param {import('discord.js').Message} referencedMessage
+ */
+module.exports.sendHugMessage = async (interaction, userData, characterData, referencedMessage) => {
 
-	botReply
-		.awaitMessageComponent({ filter, time: 120_000 })
-		.then(async interaction => {
+	if (!(interaction.message instanceof Message)) { return; }
 
-			if (interaction.customId === 'hug-decline') {
+	const hugURLs = [
+		'https://c.tenor.com/h94rl66G50cAAAAC/hug-cats.gif',
+		'https://c.tenor.com/-YZ5lgNG7ecAAAAd/yes-love.gif',
+		'https://c.tenor.com/K-mORy7U1SsAAAAd/wolf-animal.gif',
+		'https://c.tenor.com/x2Ne9xx0SBgAAAAC/funny-animals-monkey-hug.gif',
+		'https://c.tenor.com/a8H63f_WrqEAAAAC/border-collie-hug.gif',
+		'https://c.tenor.com/jQud2Zph9OoAAAAC/animal-animals.gif',
+		'https://c.tenor.com/tyK64-bjkikAAAAC/sweet-animals-cute.gif',
+		'https://c.tenor.com/K2uYNMCeqe4AAAAC/bear-hug.gif',
+		'https://c.tenor.com/j9ovpes78QsAAAAd/huge-hug-bromance.gif',
+		'https://c.tenor.com/EKlPRdcuoccAAAAC/otter-cute.gif',
+		'https://c.tenor.com/N-MAzVmbytEAAAAd/cat-dog.gif',
+		'https://c.tenor.com/WvsUTL2ocVkAAAAd/cute-cats-cuddling-cats.gif',
+		'https://c.tenor.com/8SjdZ9f64s8AAAAd/animals-kiss.gif',
+		'https://c.tenor.com/VOLRmvc9PawAAAAd/cute-animals.gif',
+		'https://c.tenor.com/N4wxlSS6s6YAAAAd/wake-up-360baby-pandas.gif',
+	];
 
-				return Promise.reject();
-			}
-
-			const hugURLs = [
-				'https://c.tenor.com/h94rl66G50cAAAAC/hug-cats.gif',
-				'https://c.tenor.com/-YZ5lgNG7ecAAAAd/yes-love.gif',
-				'https://c.tenor.com/K-mORy7U1SsAAAAd/wolf-animal.gif',
-				'https://c.tenor.com/x2Ne9xx0SBgAAAAC/funny-animals-monkey-hug.gif',
-				'https://c.tenor.com/a8H63f_WrqEAAAAC/border-collie-hug.gif',
-				'https://c.tenor.com/jQud2Zph9OoAAAAC/animal-animals.gif',
-				'https://c.tenor.com/tyK64-bjkikAAAAC/sweet-animals-cute.gif',
-				'https://c.tenor.com/K2uYNMCeqe4AAAAC/bear-hug.gif',
-				'https://c.tenor.com/j9ovpes78QsAAAAd/huge-hug-bromance.gif',
-				'https://c.tenor.com/EKlPRdcuoccAAAAC/otter-cute.gif',
-				'https://c.tenor.com/N-MAzVmbytEAAAAd/cat-dog.gif',
-				'https://c.tenor.com/WvsUTL2ocVkAAAAd/cute-cats-cuddling-cats.gif',
-				'https://c.tenor.com/8SjdZ9f64s8AAAAd/animals-kiss.gif',
-				'https://c.tenor.com/VOLRmvc9PawAAAAd/cute-animals.gif',
-				'https://c.tenor.com/N4wxlSS6s6YAAAAd/wake-up-360baby-pandas.gif',
-			];
-
-			await botReply
-				.edit({
-					embeds: [...embedArray, {
-						color: characterData?.color || message.member?.displayColor || message.author.accentColor || '#ffffff',
-						author: {
-							name: characterData?.name || message.member?.displayName || message.author?.tag,
-							icon_url: characterData?.avatarURL || message.member?.displayAvatarURL() || message.author?.avatarURL() || undefined,
-						},
-						image: { url: hugURLs[generateRandomNumber(hugURLs.length, 0)] },
-					}],
-					components: [],
-				})
-				.catch((error) => {
-					if (error.httpStatus !== 404) { throw new Error(error); }
-				});
-
-			const partnerUserData = /** @type {import('../../typedef').ProfileSchema} */ (await profileModel.findOne({ userId: firstMentionedUser.id }));
-			const partnerCharacterData = partnerUserData?.characters?.[partnerUserData?.currentCharacter?.[message.guildId || 'DM']];
-
-			if (characterData !== undefined && partnerCharacterData !== undefined) { await addFriendshipPoints(message, userData, characterData._id, partnerUserData, partnerCharacterData._id); }
+	const embedArray = interaction.message.embeds.slice(0, interaction.message.embeds.length - 1);
+	await interaction.message
+		.edit({
+			embeds: [...embedArray, {
+				color: characterData?.color || referencedMessage.member?.displayColor || referencedMessage.author.accentColor || '#ffffff',
+				author: {
+					name: characterData?.name || referencedMessage.member?.displayName || referencedMessage.author?.tag,
+					icon_url: characterData?.avatarURL || referencedMessage.member?.displayAvatarURL() || referencedMessage.author?.avatarURL() || undefined,
+				},
+				image: { url: hugURLs[generateRandomNumber(hugURLs.length, 0)] },
+			}],
+			components: [],
 		})
-		.catch(async () => {
-
-			return await botReply
-				.edit({
-					embeds: [...embedArray, {
-						color: characterData?.color || message.member?.displayColor || message.author.accentColor || '#ffffff',
-						author: {
-							name: characterData?.name || message.member?.displayName || message.author?.tag,
-							icon_url: characterData?.avatarURL || message.member?.displayAvatarURL() || message.author?.avatarURL() || undefined,
-						},
-						description:`${firstMentionedUser.toString()} did not accept the hug.`,
-					}],
-					components: disableAllComponents(botReply.components),
-				})
-				.catch((error) => {
-					if (error.httpStatus !== 404) { throw new Error(error); }
-				});
+		.catch((error) => {
+			if (error.httpStatus !== 404) { throw new Error(error); }
 		});
 
-	return;
+	const partnerUserData = /** @type {import('../../typedef').ProfileSchema} */ (await profileModel.findOne({ userId: referencedMessage.mentions.users.first()?.id }));
+	const partnerCharacterData = partnerUserData?.characters?.[partnerUserData?.currentCharacter?.[referencedMessage.guildId || 'DM']];
+
+	if (characterData !== undefined && partnerCharacterData !== undefined) { await addFriendshipPoints(referencedMessage, userData, characterData._id, partnerUserData, partnerCharacterData._id); }
+};
+
+/**
+ * A function that is called when the user declines the hug. It edits the message to show that the user
+ * declined the hug.
+ * @param {import('discord.js').ButtonInteraction} interaction
+ * @param {import('../../typedef').Character} characterData
+ * @param {import('discord.js').Message} referencedMessage
+ */
+module.exports.sendNoHugMessage = async (interaction, characterData, referencedMessage) => {
+
+	if (!(interaction.message instanceof Message)) { return; }
+
+	const embedArray = interaction.message.embeds.slice(0, interaction.message.embeds.length - 1);
+	await interaction.message
+		.edit({
+			embeds: [...embedArray, {
+				color: characterData?.color || referencedMessage.member?.displayColor || referencedMessage.author.accentColor || '#ffffff',
+				author: {
+					name: characterData?.name || referencedMessage.member?.displayName || referencedMessage.author?.tag,
+					icon_url: characterData?.avatarURL || referencedMessage.member?.displayAvatarURL() || referencedMessage.author?.avatarURL() || undefined,
+				},
+				description:`${referencedMessage.mentions.users.first()?.toString()} did not accept the hug.`,
+			}],
+			components: disableAllComponents(interaction.message.components),
+		})
+		.catch((error) => {
+			if (error.httpStatus !== 404) { throw new Error(error); }
+		});
 };
