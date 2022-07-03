50a2b5f067565e2c53c33bdcf5ec99a31d272bc6
maksirose <maksilucy1@gmail.com>
2022-07-03 23:27:04 +0200

    Replace events

---
+++ src/handlers/events.ts
@@ -0,0 +1,20 @@
+import { readdirSync } from 'fs';
+import { CustomClient, Event } from '../typedef';
+
+/** Starts up all the `listeners` based on their `eventName` and whether or not their are one-time. */
+export function execute(client: CustomClient) {
+
+	for (const file of readdirSync('./dist/events/')) {
+
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		const event: Event = require(`../events/${file}`);
+		if (event.once) {
+
+			client.once(event.name, (...args) => event.execute(client, ...args));
+		}
+		else {
+
+			client.on(event.name, (...args) => event.execute(client, ...args));
+		}
+	}
+}
