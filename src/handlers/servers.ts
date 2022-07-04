fe7aef7b73e85c63db84f14d67dff4d2fe183ad6
maksirose <maksilucy1@gmail.com>
2022-07-04 11:36:19 +0200

    Update server file

---
+++ src/handlers/servers.ts
@@ -0,0 +1,19 @@
+import { readdirSync } from 'fs';
+import serverModel from '../models/serverModel';
+import { ServerSchema } from '../typedef';
+
+export async function execute() {
+
+	const serverFiles = readdirSync('./database/servers').filter(file => file.endsWith('.json'));
+
+	/* This updates each server to set currentlyVisiting to null. */
+	for (const file of serverFiles) {
+
+		await serverModel.findOneAndUpdate(
+			{ uuid: file.replace('.json', '') },
+			(s: ServerSchema) => {
+				s.currentlyVisiting = null;
+			},
+		);
+	}
+}
