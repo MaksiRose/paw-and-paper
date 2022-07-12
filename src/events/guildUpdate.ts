f7fc49e5386a32a27ab7cf51e997be5bf369cb44
maksirose <maksilucy1@gmail.com>
2022-07-12 20:06:24 +0200

    Update guildUpdate

---
+++ src/events/guildUpdate.ts
@@ -0,0 +1,17 @@
+import { Guild } from 'discord.js';
+import serverModel from '../models/serverModel';
+import { CustomClient, Event } from '../typedef';
+
+export const event: Event = {
+	name: 'guildUpdate',
+	once: false,
+	async execute(client: CustomClient, oldGuild: Guild, newGuild: Guild) {
+
+		serverModel.findOneAndUpdate(
+			{ serverId: newGuild.id },
+			(s) => {
+				s.name = newGuild.name;
+			},
+		);
+	},
+};
