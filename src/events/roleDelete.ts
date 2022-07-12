e2a61e74837c4b4b81c4a15ce90d125386d7b570
maksirose <maksilucy1@gmail.com>
2022-07-12 20:19:41 +0200

    Update roleDelete

---
+++ src/events/roleDelete.ts
@@ -0,0 +1,48 @@
+import { Role } from 'discord.js';
+import serverModel from '../models/serverModel';
+import userModel from '../models/userModel';
+import { CustomClient, Event } from '../typedef';
+
+export const event: Event = {
+	name: 'roleDelete',
+	once: false,
+	async execute(client: CustomClient, role: Role) {
+
+		const serverData = serverModel.findOne({ serverId: role.guild.id });
+		const roles = serverData.shop.filter(shoprole => shoprole.roleId === role.id);
+
+		for (const shoprole of roles) {
+
+			const allServerUsers = userModel.find(
+				(u) => Object.keys(u.currentCharacter).includes(role.guild.id),
+			);
+
+			for (const user of allServerUsers) {
+
+				const characters = Object.values(user.characters).filter(c => Object.keys(c.profiles).includes(role.guild.id));
+
+				for (const character of characters) {
+
+					const profile = character.profiles[role.guild.id];
+					const userRoleIndex = profile.roles.findIndex(profilerole => profilerole.roleId === shoprole.roleId && profilerole.wayOfEarning === shoprole.wayOfEarning && profilerole.requirement === shoprole.requirement);
+
+					if (userRoleIndex >= 0) {
+
+						const userRole = profile.roles[userRoleIndex];
+						userModel.findOneAndUpdate(
+							{ uuid: user.uuid },
+							(u) => {
+								u.characters[character._id].profiles[profile.serverId].roles.splice(userRoleIndex, 1);
+
+								if (userRole.wayOfEarning === 'experience') {
+
+									u.characters[character._id].profiles[profile.serverId].experience += (Number(userRole.requirement) || 0);
+								}
+							},
+						);
+					}
+				}
+			}
+		}
+	},
+};
