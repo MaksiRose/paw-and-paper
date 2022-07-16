7e99b5b9b47d7ede4230d34abdcc1b43f3a3e6e3
maksirose <maksilucy1@gmail.com>
2022-07-16 03:43:24 +0200

    Update levelHandling util to ts

--- src/utils/checkValidity.ts
+++ src/utils/checkValidity.ts
@@ -1,9 +1,10 @@
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, MessageEmbed } from 'discord.js';
 import { respond } from '../events/interactionCreate';
 import userModel from '../models/userModel';
 import { pronoun, pronounAndPlural } from './getPronouns';
+import { decreaseLevel } from './levelHandling';
 
-export async function isPassedOut(interaction: CommandInteraction, uuid: string, isNew: boolean): Promise<boolean> {
+export async function isPassedOut(interaction: CommandInteraction<'cached' | 'raw'>, uuid: string, isNew: boolean): Promise<boolean> {
 
 	const userData = await userModel.findOne({ uuid: uuid }).catch(() => { return null; });
 	const characterData = userData?.characters?.[userData.currentCharacter?.[interaction.guildId || 'DMs']];
@@ -12,18 +13,14 @@ export async function isPassedOut(interaction: CommandInteraction, uuid: string,
 	if (userData && characterData && profileData && (profileData.energy <= 0 || profileData.health <= 0 || profileData.hunger <= 0 || profileData.thirst <= 0)) {
 
 		const botReply = await respond(interaction, {
-			embeds: [{
+			embeds: [ new MessageEmbed({
 				color: characterData.color,
 				author: { name: characterData.name, icon_url: characterData.avatarURL },
 				description: `*${characterData.name} lies on the ground near the pack borders, barely awake.* "Healer!" *${pronounAndPlural(characterData, 0, 'screeches', 'screech')} with ${pronoun(characterData, 2)} last energy. Without help, ${pronoun(characterData, 0)} will not be able to continue.*`,
-			}],
+				footer: isNew ? { text: await decreaseLevel(userData, interaction) } : undefined,
+			})],
 		}, true)
 			.catch((error) => { throw new Error(error); });
-
-		if (isNew === true) {
-
-			await decreaseLevel(userData, botReply);
-		}
 
 		await passingoutAdvice(message, userData);
 
--- src/utils/levelHandling.ts
+++ src/utils/levelHandling.ts
@@ -1,9 +1,14 @@
 import { CommandInteraction, Message, MessageEmbed } from 'discord.js';
+import { respond } from '../events/interactionCreate';
 import userModel from '../models/userModel';
 import { ServerSchema, UserSchema } from '../typedef';
-import { checkLevelRequirements } from './checkRoleRequirements';
+import { checkLevelRequirements, checkRoleCatchBlock } from './checkRoleRequirements';
 import { upperCasePronounAndPlural } from './getPronouns';
+const { default_color } = require('../../config.json');
 
+/**
+ * Checks if the user is eligable for a level up, and sends a message if so.
+ */
 export async function checkLevelUp(interaction: CommandInteraction<'cached' | 'raw'>, userData: UserSchema, serverData: ServerSchema, botReply?: Message): Promise<Message | undefined> {
 
 	/* Getting characterData and profileData */
@@ -37,13 +42,102 @@ export async function checkLevelUp(interaction: CommandInteraction<'cached' | 'r
 				});
 		}
 
-
 		botReply = await checkLevelUp(interaction, userData, serverData, botReply);
 
 		const guild = interaction.guild || await interaction.client.guilds.fetch(interaction.guildId);
 		const member = await guild.members.fetch(userData.userId);
-		await checkLevelRequirements(serverData, message, member, profileData.levels);
+		await checkLevelRequirements(serverData, interaction, member, profileData.levels);
 	}
 
 	return botReply;
+}
+
+/**
+ * Decreases the users level based on their current levels and removes their inventory, returns footerText for an updated bot reply.
+ */
+export async function decreaseLevel(userData: UserSchema, interaction: CommandInteraction<'cached' | 'raw'>): Promise<string> {
+
+	/* Defining characterData and profileData */
+	let characterData = userData.characters[userData.currentCharacter[interaction.guildId]];
+	let profileData = characterData.profiles[interaction.guildId];
+
+	/* newUserLevel is nine tenths of current profile level. */
+	const newUserLevel = Math.round(profileData.levels - (profileData.levels / 10));
+
+	/* footerText displays how many levels are lost, if any, and how much experience is lost, if any. */
+	let footerText = '';
+	if (newUserLevel !== profileData.levels) { footerText += `-${profileData.levels - newUserLevel} level${(profileData.levels - newUserLevel > 1) ? 's' : ''}\n`; }
+	if (profileData.experience > 0) { footerText += `-${profileData.experience} XP`; }
+
+	/* Updating the footerText and the database for any items the user had. */
+	for (const itemType of Object.keys(profileData.inventory)) {
+
+		for (const item of Object.keys(profileData.inventory[itemType])) {
+
+			if (profileData.inventory[itemType][item] > 0) {
+
+				footerText += `\n-${profileData.inventory[itemType][item]} ${item}`;
+				profileData.inventory[itemType][item] = 0;
+			}
+		}
+	}
+
+	userData = await userModel.findOneAndUpdate(
+		{ uuid: userData.uuid },
+		(u) => {
+			u.characters[u.currentCharacter[interaction.guildId]].profiles[interaction.guildId].levels = newUserLevel;
+			u.characters[u.currentCharacter[interaction.guildId]].profiles[interaction.guildId].experience = 0;
+			u.characters[u.currentCharacter[interaction.guildId]].profiles[interaction.guildId].inventory = profileData.inventory;
+		},
+	);
+	characterData = userData.characters[userData.currentCharacter[interaction.guildId]];
+	profileData = characterData.profiles[interaction.guildId];
+
+
+	/* Get the guild, member, and the profileData roles where the wayOfEarning is levels and the role requirement bigger than the profile level. */
+	const guild = interaction.guild || await interaction.client.guilds.fetch(interaction.guildId);
+	const member = await guild.members.fetch(userData.userId);
+	const roles = profileData.roles.filter(role => role.wayOfEarning === 'levels' && role.requirement > profileData.levels);
+
+	/* It's checking if the user has any roles that are earned by leveling up, and if they do, it will remove them from the user's profileData.roles and remove the role from the user. */
+	for (const role of roles) {
+
+		try {
+
+			/* It's removing the role from the user's profileData.roles. */
+			const userRoleIndex = profileData.roles.indexOf(role);
+			if (userRoleIndex >= 0) { profileData.roles.splice(userRoleIndex, 1); }
+
+			await userModel.findOneAndUpdate(
+				{ uuid: userData.uuid },
+				(u) => {
+					u.characters[u.currentCharacter[interaction.guildId]].profiles[interaction.guildId].roles = profileData.roles;
+				},
+			);
+
+			/* It's checking if the user has the role, and if they do, it will remove it and send a message to the user. */
+			if (member.roles.cache.has(role.roleId)) {
+
+				await member.roles.remove(role.roleId);
+
+				await respond(interaction, {
+					content: member.toString(),
+					embeds: [{
+						color: default_color,
+						author: { name: guild.name, icon_url: guild.iconURL() || undefined },
+						description: `You lost the <@&${role.roleId}> role because of a lack of levels!`,
+					}],
+				}, false)
+					.catch((error) => {
+						if (error.httpStatus !== 404) { throw new Error(error); }
+					});
+			}
+		}
+		catch (error) {
+
+			await checkRoleCatchBlock(error, interaction, member);
+		}
+	}
+
+	return footerText;
 }
