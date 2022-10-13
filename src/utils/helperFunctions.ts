595ca97d3f928c6978793b7234a04b8966466d79
maksirose <maksilucy1@gmail.com>
2022-10-13 21:10:26 +0200

    fix: add another err 400 catcher

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -202,6 +202,11 @@ export async function sendErrorMessage(
 		console.error('Error 404 - An error is not being sent to the user. ', error);
 		return;
 	}
+	else if ((objectHasKey(error, 'status') && error.status === 400) || (objectHasKey(error, 'httpStatus') && error.httpStatus === 400)) {
+
+		console.error('Error 400 - An error is not being sent to the user. ', error);
+		return;
+	}
 	const isECONNRESET = objectHasKey(error, 'code') && error.code === 'ECONNRESET';
 	console.error(error, jsonInteraction);
 
