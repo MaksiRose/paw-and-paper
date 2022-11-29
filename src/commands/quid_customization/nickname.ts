b3a29c1fd94e5758f206102dfd0030d49ac9855e
MaksiRose <maksilucy1@gmail.com>
2022-11-30 00:27:46 +0100

    fix: remove deprecations from nickname command

--- src/commands/quid_customization/nickname.ts
+++ src/commands/quid_customization/nickname.ts
@@ -1,7 +1,7 @@
 import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { SlashCommand } from '../../typings/handle';
 import { hasName } from '../../utils/checkUserState';
-import { getMapData, reply } from '../../utils/helperFunctions';
+import { getMapData, respond } from '../../utils/helperFunctions';
 
 export const command: SlashCommand = {
 	data: new SlashCommandBuilder()
@@ -18,25 +18,26 @@ export const command: SlashCommand = {
 	modifiesServerProfile: false,
 	sendCommand: async (interaction, userData, serverData) => {
 
-		if (!hasName(userData, interaction)) { return; }
+		if (!hasName(userData, interaction)) { return; } // This is always a reply
 
 		const nickname = interaction.options.getString('nickname') || '';
 
 		await userData.update(
 			(u) => {
-				const q = getMapData(u.quids, getMapData(u.currentQuid, interaction.guildId || 'DMs'));
+				const q = getMapData(u.quids, getMapData(u.servers, interaction.guildId || 'DMs').currentQuid ?? '');
 				if (serverData) { q.nickname.servers[serverData.serverId] = nickname; }
 				else { q.nickname.global = nickname; }
 			},
 		);
 
-		await reply(interaction, {
+		// This is always a reply
+		await respond(interaction, {
 			embeds: [new EmbedBuilder()
 				.setColor(userData.quid.color)
 				.setAuthor({ name: userData.quid.getDisplayname(), iconURL: userData.quid.avatarURL })
 				.setTitle(serverData ? `Nickname for ${userData.quid.name} ${userData.quid.nickname.server ? `set to ${userData.quid.nickname.server}` : 'removed'} in ${serverData.name}!` : `Nickname for ${userData.quid.name} ${userData.quid.nickname.global ? `set to ${userData.quid.nickname.global}` : 'removed'} globally!`)
 				.setDescription(serverData ? 'Tip: Nicknames can be set globally too by executing the command in DMs. The global nickname will be displayed when no server-specific nickname has been chosen.' : 'Tip: Nicknames can be set server-specific too by executing the command in the server. The server-specific nickname will overwrite the global nickname for that server.')],
-		}, true);
+		});
 		return;
 	},
 };
