e1d8afb59c30e5101ab793dd0360b7f59e25ee93
MaksiRose <maksilucy1@gmail.com>
2023-02-26 19:58:14 +0100

    test typings

--- src/models/proxyLimits.ts
+++ src/models/proxyLimits.ts
@@ -1,8 +1,18 @@
+import { Optional } from 'sequelize';
 import { Column, DataType, HasOne, Model, Table } from 'sequelize-typescript';
 import Server from './server';
 
+interface ProxyLimitsAttributes {
+	id: number;
+	setToWhitelist: boolean;
+	whitelist: string[];
+	blacklist: string[];
+}
+
+type ProxyLimitsCreationAttributes = Optional<ProxyLimitsAttributes, 'id'>;
+
 @Table
-export default class ProxyLimits extends Model {
+export default class ProxyLimits extends Model<ProxyLimitsAttributes, ProxyLimitsCreationAttributes> {
 	@Column({ type: DataType.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true })
 	declare id: number;
 
