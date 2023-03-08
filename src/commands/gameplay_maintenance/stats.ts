b07809d99b5d828c19f0cf135d16c815310b10be
MaksiRose <maksilucy1@gmail.com>
2023-03-08 20:08:02 +0100

    feat: update stats cmd

--- src/commands/gameplay_maintenance/stats.ts
+++ src/commands/gameplay_maintenance/stats.ts
@@ -1,11 +1,14 @@
 import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
-import { userModel, getUserData } from '../../oldModels/userModel';
-import { RankType, UserData } from '../../typings/data/user';
+import DiscordUser from '../../models/discordUser';
+import Quid from '../../models/quid';
+import QuidToServer from '../../models/quidToServer';
+import User from '../../models/user';
+import UserToServer from '../../models/userToServer';
+import { RankType } from '../../typings/data/user';
 import { SlashCommand } from '../../typings/handle';
 import { hasNameAndSpecies, isInGuild } from '../../utils/checkUserState';
 import { isInvalid } from '../../utils/checkValidity';
-import { getArrayElement, getMapData, respond } from '../../utils/helperFunctions';
-import { calculateInventorySize } from '../../utils/simulateItemUse';
+import { getArrayElement, respond } from '../../utils/helperFunctions';
 import { sendStoreMessage } from './store';
 const { error_color } = require('../../../config.json');
 
@@ -23,19 +26,41 @@ export const command: SlashCommand = {
 	position: 0,
 	disablePreviousCommand: false,
 	modifiesServerProfile: false,
-	sendCommand: async (interaction, { user, quid, userToServer, quidToServer }) => {
+	sendCommand: async (interaction, { user, quid, userToServer, quidToServer, discordUser, server }) => {
 
 		/* This ensures that the user is in a guild and has a completed account. */
-		if (!isInGuild(interaction)) { return; } // This is always a reply
+		if (!isInGuild(interaction) || !server) { return; } // This is always a reply
 
 		const mentionedUser = interaction.options.getUser('user');
+
+		discordUser = (!mentionedUser || mentionedUser.id === interaction.user.id) ? discordUser : await DiscordUser.findByPk(interaction.user.id, {
+			include: [{ model: User, as: 'user' }],
+		}) ?? undefined;
+
+		const isYourself = (user?.id === discordUser?.user.id);
+		user = discordUser?.user;
+
+		userToServer = isYourself ? userToServer : (user && server)
+			? (await UserToServer.findOne({
+				where: { userId: user.id, serverId: server.id },
+				include: [{ model: Quid, as: 'activeQuid' }],
+			})) ?? undefined
+			: undefined;
+
+		quid = isYourself ? quid : (user && !server)
+			? (user.lastGlobalActiveQuidId ? ((await Quid.findByPk(user.lastGlobalActiveQuidId)) ?? undefined) : undefined)
+			: (userToServer?.activeQuid ?? undefined);
+
+		quidToServer = isYourself ? quidToServer : (quid && interaction.inGuild())
+			? (await QuidToServer.findOne({
+				where: { quidId: quid.id, serverId: interaction.guildId },
+			})) ?? undefined
+			: undefined;
+
+
 		if (mentionedUser) {
 
-			const _userData = (() => {
-				try { return userModel.findOne(u => Object.keys(u.userIds).includes(mentionedUser.id)); }
-				catch { return null; }
-			})();
-			if (!_userData) {
+			if (!user || !discordUser) {
 
 				// This is always a reply
 				await respond(interaction, {
@@ -47,9 +72,7 @@ export const command: SlashCommand = {
 				});
 				return;
 			}
-
-			userData = getUserData(_userData, interaction.guildId, getMapData(_quids, getMapData(_userData.servers, interaction.guildId).currentQuid ?? ''));
-			if (!hasNameAndSpecies(userData)) {
+			else if (!hasNameAndSpecies(quid)) {
 
 				// This is always a reply
 				await respond(interaction, {
@@ -62,15 +85,16 @@ export const command: SlashCommand = {
 				return;
 			}
 		}
-		else if (!hasNameAndSpecies(quid, { interaction, hasQuids: quid !== undefined || (await Quid.count({ where: { userId: user.id } })) > 0 })) { return; } // This is always a reply
+		else if (!hasNameAndSpecies(quid, { interaction, hasQuids: false })) { return; } // This is always a reply
 
-		await sendStatsMessage(interaction, userData, interaction.user.id);
+		if (!quidToServer) { throw new TypeError('quidToServer is undefined'); }
+		await sendStatsMessage(interaction, quid, quidToServer, interaction.user.id, isYourself);
 	},
 	async sendMessageComponentResponse(interaction, { user, quid, userToServer, quidToServer, server }) {
 
 		if (!interaction.isButton()) { return; }
 		/* This ensures that the user is in a guild and has a completed account. */
-		if (serverData === null) { throw new Error('serverData is null'); }
+		if (server === null) { throw new Error('serverData is null'); }
 		if (!isInGuild(interaction)) { return; } // This is always a reply
 
 		if (interaction.customId.includes('refresh')) {
@@ -78,29 +102,39 @@ export const command: SlashCommand = {
 			const quidId = getArrayElement(interaction.customId.split('_'), 2);
 			const creatorUserId = getArrayElement(interaction.customId.split('_'), 3).replace('@', '');
 
-			const _userData = await userModel.findOne(u => Object.keys(u.quids).includes(quidId));
-			userData = getUserData(_userData, interaction.guildId, getMapData(_quids, quidId));
-			if (!hasNameAndSpecies(userData)) { throw Error('quid.species is empty string'); }
-			await sendStatsMessage(interaction, userData, creatorUserId);
+			quid = await Quid.findByPk(quidId, { rejectOnEmpty: true });
+			const isYourself = (quid.userId === creatorUserId);
+
+			quidToServer = await QuidToServer.findOne({
+				where: { quidId: quid.id, serverId: interaction.guildId },
+				rejectOnEmpty: true,
+			});
+
+			await sendStatsMessage(interaction, quid, quidToServer, creatorUserId, isYourself);
 			return;
 		}
 
 		if (interaction.customId.includes('store')) {
 
+			if (!user) { throw new TypeError('user is undefined'); }
+			if (!userToServer) { throw new TypeError('userToServer is undefined'); }
 			if (!hasNameAndSpecies(quid, { interaction, hasQuids: quid !== undefined || (await Quid.count({ where: { userId: user.id } })) > 0 })) { return; }
+			if (!quidToServer) { throw new TypeError('quidToServer is undefined'); }
+
 			const restEmbed = await isInvalid(interaction, user, userToServer, quid, quidToServer);
 			if (restEmbed === false) { return; }
 
 			await sendStoreMessage(interaction, userData, serverData, restEmbed);
 		}
-
 	},
 };
 
 async function sendStatsMessage(
 	interaction: ChatInputCommandInteraction<'cached'> | ButtonInteraction<'cached'>,
-	userData: UserData<never, never>,
+	quid: Quid,
+	quidToServer: QuidToServer,
 	creatorUserId: string,
+	isYourself: boolean,
 ): Promise<void> {
 
 	const components = new ActionRowBuilder<ButtonBuilder>()
@@ -114,7 +148,7 @@ async function sendStatsMessage(
 			.setStyle(ButtonStyle.Secondary),
 		]);
 
-	if (calculateInventorySize(quidToServer.inventory) === 0 || !Object.keys(userData.userIds).includes(creatorUserId)) {
+	if (quidToServer.inventory.length === 0 || !isYourself) {
 
 		components.components.pop();
 	}
@@ -122,7 +156,14 @@ async function sendStatsMessage(
 	// "item" needs to be == and not === in order to catch the booleans as well
 	let injuryText = '';
 
-	for (const [injuryKind, injuryAmount] of Object.entries(quidToServer.injuries)) {
+	const injuries: [string, number | boolean][] = [
+		['wounds', quidToServer.injuries_wounds],
+		['infections', quidToServer.injuries_infections],
+		['cold', quidToServer.injuries_cold],
+		['sprains', quidToServer.injuries_sprains],
+		['poison', quidToServer.injuries_poison],
+	];
+	for (const [injuryKind, injuryAmount] of injuries) {
 
 		if (injuryAmount > 0) {
 
@@ -146,7 +187,7 @@ async function sendStatsMessage(
 			`❤️ HP: \`${quidToServer.health}/${quidToServer.maxHealth}\` - ⚡ Energy: \`${quidToServer.energy}/${quidToServer.maxEnergy}\`\n` +
 			`🍗 Hunger: \`${quidToServer.hunger}/${quidToServer.maxHunger}\` - 🥤 Thirst: \`${quidToServer.thirst}/${quidToServer.maxThirst}\`` +
 			(injuryText ? `\n🩹 Injuries/Illnesses: ${injuryText.slice(2)}` : injuryText) +
-			(quidToServer.sapling.exists === false ? '' : `\n🌱 Ginkgo Sapling: ${quidToServer.sapling.waterCycles} days alive - ${quidToServer.sapling.health} health - Next watering <t:${Math.floor((quidToServer.sapling.nextWaterTimestamp || 0) / 1000)}:R>`) +
+			(quidToServer.sapling_exists === false ? '' : `\n🌱 Ginkgo Sapling: ${quidToServer.sapling_waterCycles} days alive - ${quidToServer.sapling_health} health - Next watering <t:${Math.floor((quidToServer.sapling_nextWaterTimestamp || 0) / 1000)}:R>`) +
 			(quidToServer.hasQuest ? `\n${quid.name} has one open quest!` : '') + (canRankUp ? `\n${quid.name} can rank up!` : ''),
 		components: [components],
 	}, 'update', interaction.isMessageComponent() ? interaction.message.id : undefined);
