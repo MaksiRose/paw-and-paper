c505d219b93218fa9363fa3aa346c8c7ce273486
MaksiRose <maksilucy1@gmail.com>
2023-05-08 16:11:59 +0200

    fix: resolve #795

--- src/contextmenu/edit.ts
+++ src/contextmenu/edit.ts
@@ -35,7 +35,18 @@ export const command: ContextMenuCommand = {
 				model: Quid, as: 'quid', attributes: ['userId'],
 			}],
 		});
-		const discordUsers = await DiscordUser.findAll({ where: { userId: webhookData?.quid?.userId } }) ?? [];
+
+		if (!webhookData) {
+
+			await interaction
+				.reply({
+					content: 'With this command, you can edit a proxied message you sent. Either the selected message is not a proxied message, or it hasn\'t been proxied by this bot.',
+					ephemeral: true,
+				});
+			return;
+		}
+
+		const discordUsers = await DiscordUser.findAll({ where: { userId: webhookData.quid.userId } }) ?? [];
 
 		/* This is checking if the user who is trying to delete the message is the same user who sent the message. */
 		if (!discordUsers.some(du => du.id === interaction.user.id)) {
